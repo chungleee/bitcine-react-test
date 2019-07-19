b9f8138dcb4c094f24f2f258ef50f0fb677c14c0
Leon <leon.karho.chung@gmail.com>
2019-07-19 15:54:53 -0400

    setup redux boilerplate: Provider + create store + redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
+
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const initialState = {}
+const middlewares = [thunk]
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middlewares),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
