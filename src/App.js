b97345649caf9f3910ee22c53c0f3c605a5c92c5
Leon <leon.karho.chung@gmail.com>
2019-07-20 16:13:36 -0400

    implement basic loading indicator, map starships props and pass data as props to Starship component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchStarships } from './actions/starshipsActions'
+import Starship from './components/presentational/Starship'
 
 class App extends Component {
   componentDidMount() {
     this.props.fetchStarships()
   }
-
   render() {
-    return <div>hello</div>
+    const ships = this.props.starships.starships.results
+
+    if (ships === undefined) {
+      return <div>...loading</div>
+    } else {
+      return (
+        <ul>
+          {ships.map((ship, idx) => {
+            return (
+              <Starship
+                key={idx}
+                name={ship.name}
+                model={ship.model}
+                manufacturer={ship.manufacturer}
+              />
+            )
+          })}
+        </ul>
+      )
+    }
   }
 }
 
