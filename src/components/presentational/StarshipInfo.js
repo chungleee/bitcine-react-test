058b81655d886a582deafef4fc8b28a0047c3b7e
Leon <leon.karho.chung@gmail.com>
2019-07-21 17:18:14 -0400

    grab state passed down from Link -> iterate over object and display data && a back button for better ux

--- src/components/presentational/StarshipInfo.js
+++ src/components/presentational/StarshipInfo.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
-const StarshipInfo = ({ match }) => {
-  return <div>{match.params.name}</div>
+const StarshipInfo = ({ history, location, match }) => {
+  const info = location.state.ship
+  return (
+    <div>
+      <button
+        onClick={() => {
+          history.goBack()
+        }}
+      >
+        Back
+      </button>
+      <h1>{match.params.name}</h1>
+      {Object.keys(info).map(key => {
+        return <div>{`${key}: ${info[key]}`}</div>
+      })}
+    </div>
+  )
 }
 
 export default StarshipInfo
