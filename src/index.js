64688f8c95c695ca4803f9a5ac9fe75f03e79227
Leon <leon.karho.chung@gmail.com>
2019-07-21 15:14:51 -0400

    setup react router at root

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware, compose } from 'redux'
@@ -7,6 +8,7 @@ import { Provider } from 'react-redux'
 import rootReducer from './reducers'
 
 import App from './App'
+import StarshipInfo from './components/presentational/StarshipInfo'
 
 const initialState = {}
 const middlewares = [thunk]
@@ -22,7 +24,10 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path="/" exact component={App} />
+      <Route path="/:name" component={StarshipInfo} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
