0eeae10a36560a8d3def31f5b74eecca50d24985
Leon <leon.karho.chung@gmail.com>
2019-07-21 15:19:15 -0400

    set up Link: dynamic url and pass ship into Link state

--- src/components/presentational/Starship.js
+++ src/components/presentational/Starship.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Starship = ({ name, model, manufacturer }) => {
+const Starship = ({ ship }) => {
+  const { name, model, manufacturer } = ship
   return (
     <li>
-      <p>Name: {name}</p>
+      <p>
+        Name:
+        <Link
+          to={{
+            pathname: `/${name}`,
+            state: { ship }
+          }}
+        >
+          {name}
+        </Link>
+      </p>
       <p>Model: {model}</p>
       <p>Manufacturer: {manufacturer}</p>
     </li>
