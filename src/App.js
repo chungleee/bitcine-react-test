aee0775d5822a5054cf8e1bc9ceeba2bf6b69d9f
Leon <leon.karho.chung@gmail.com>
2019-07-21 17:20:47 -0400

    move logic back to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import React from 'react'
-import StarshipsList from './components/container/StarshipsList'
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { fetchStarships } from './actions/starshipsActions'
+import StarshipsList from './components/presentational/StarshipsList'
 
-const App = () => {
-  return <StarshipsList />
+class App extends Component {
+  componentDidMount() {
+    const url = 'https://swapi.co/api/starships/'
+    this.props.fetchStarships(url)
+  }
+
+  render() {
+    const previous = this.props.starships.starships.previous
+    const next = this.props.starships.starships.next
+    return (
+      <Fragment>
+        <div>
+          <button
+            onClick={() => {
+              this.props.fetchStarships(previous)
+            }}
+            disabled={previous === null}
+          >
+            Prev
+          </button>
+          <button
+            onClick={() => {
+              this.props.fetchStarships(next)
+            }}
+            disabled={next === null}
+          >
+            Next
+          </button>
+        </div>
+        <StarshipsList starships={this.props.starships} />
+      </Fragment>
+    )
+  }
 }
 
-export default App
+const mapStateToProps = state => {
+  return {
+    starships: state.starships
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchStarships }
+)(App)
