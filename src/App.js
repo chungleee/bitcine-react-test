e4d2fde463f38cfbf0d7889562328b69ba4258d9
Leon <leon.karho.chung@gmail.com>
2019-07-21 15:17:31 -0400

    move logic into StarshipsList container

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { fetchStarships } from './actions/starshipsActions'
-import Starship from './components/presentational/Starship'
+import React from 'react'
+import StarshipsList from './components/container/StarshipsList'
 
-class App extends Component {
-  componentDidMount() {
-    this.props.fetchStarships()
-  }
-  render() {
-    const ships = this.props.starships.starships.results
-
-    if (ships === undefined) {
-      return <div>...loading</div>
-    } else {
-      return (
-        <ul>
-          {ships.map((ship, idx) => {
-            return (
-              <Starship
-                key={idx}
-                name={ship.name}
-                model={ship.model}
-                manufacturer={ship.manufacturer}
-              />
-            )
-          })}
-        </ul>
-      )
-    }
-  }
+const App = () => {
+  return <StarshipsList />
 }
 
-const mapStateToProps = state => {
-  return {
-    starships: state.starships
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  { fetchStarships }
-)(App)
+export default App
