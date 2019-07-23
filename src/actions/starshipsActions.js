16f7a96946ebe4f887d0aba86d342e559a2d79e1
Leon <leon.karho.chung@gmail.com>
2019-07-23 16:04:02 -0400

    rm console.log

--- src/actions/starshipsActions.js
+++ src/actions/starshipsActions.js
@@ -4,7 +4,7 @@ const fetchStarships = url => {
   return async dispatch => {
     const res = await fetch(url)
     const data = await res.json()
-    console.log('data from action', data)
+
     return dispatch({
       type: FETCH_STARSHIPS,
       payload: data
