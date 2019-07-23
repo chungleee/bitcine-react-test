be06a8d86074ee509a778af54e102eebe909d5bf
Leon <leon.karho.chung@gmail.com>
2019-07-23 19:03:20 -0400

    move css into a styles obj for better readability

--- src/components/presentational/Starship.js
+++ src/components/presentational/Starship.js
@@ -1,19 +1,22 @@
 /** @jsx jsx */
 import React from 'react'
-import { css, jsx } from '@emotion/core'
+import { jsx } from '@emotion/core'
 import { Link } from 'react-router-dom'
 
 const Starship = ({ ship }) => {
   const { name, model, manufacturer } = ship
+
+  const styles = {
+    li: {
+      listStyle: 'none',
+      margin: '1rem 0',
+      padding: '1rem',
+      boxShadow: '0px 0px 4px 2px rgba( 0, 0, 0, 0.2 )'
+    }
+  }
+
   return (
-    <li
-      css={{
-        listStyle: 'none',
-        margin: '1rem 0',
-        padding: '1rem',
-        boxShadow: '0px 0px 4px 2px rgba( 0, 0, 0, 0.2 )'
-      }}
-    >
+    <li css={styles.li}>
       <p>
         Name:
         <Link
